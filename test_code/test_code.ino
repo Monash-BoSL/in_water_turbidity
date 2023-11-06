e70d5a40f4ce58dadad342e5114ae6523f3fd965
William Zhang <wzha0123@student.monash.edu>
2023-11-06 16:44:20 +1100

    Improved ground/power plane and better silk text
    Improved access to ground and power plane by reducing thin bottlenecks.
    
    Fixed capacitor placement for certain parts.

--- test_code/test_code.ino
+++ test_code/test_code.ino
@@ -1,9 +1,11 @@
+#include <digitalWriteFast.h>
+
 // TIA_PWR_EN (PC3/A3) -> HIGH
 // REF_EN (PC1/A1) -> HIGH
 // LED_1_EN (PD6/6) -> HIGH
 // LED_2_EN (PD7/7) -> HIGH
 // LED_DIGITAL_SIGNAL (PC2/A2) -> ON/OFF
-// LED_PWR_EN (PD4/4) -> LOW
+// LED_PWR_EN (PD4/4) -> HIGH
 // NEG_VOLT_EN (PD2/2) -> HIGH
 
 #define TIA_PWR_EN A3
@@ -13,6 +15,13 @@
 #define LED_DIGITAL_SIGNAL A2
 #define LED_PWR_EN 4
 #define NEG_VOLT_EN 2
+#define ADC_SS 10
+
+#include <Wire.h>
+#include <Adafruit_TMP117.h>
+#include <Adafruit_Sensor.h>
+
+Adafruit_TMP117  tmp117;
 
 void setup() {
   // put your setup code here, to run once:
@@ -20,23 +29,41 @@ void setup() {
   pinMode(REF_EN, OUTPUT);
   pinMode(LED_1_EN, OUTPUT);
   pinMode(LED_2_EN, OUTPUT);
-  pinMode(LED_DIGITAL_SIGNAL, OUTPUT);
+  pinModeFast(LED_DIGITAL_SIGNAL, OUTPUT);
   pinMode(LED_PWR_EN, OUTPUT);
   pinMode(NEG_VOLT_EN, OUTPUT);
+  pinMode(ADC_SS, OUTPUT);
 
   digitalWrite(TIA_PWR_EN, HIGH);
   digitalWrite(REF_EN, HIGH);
   digitalWrite(LED_1_EN, HIGH);
-  digitalWrite(LED_2_EN, HIGH);
-  digitalWrite(LED_DIGITAL_SIGNAL, HIGH);
+  digitalWrite(LED_2_EN, HIGH);  
   digitalWrite(LED_PWR_EN, LOW);
   digitalWrite(NEG_VOLT_EN, HIGH);
+  digitalWrite(ADC_SS, LOW);
+  digitalWriteFast(LED_DIGITAL_SIGNAL, HIGH);
+
+  Serial.begin(9600);
+  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
+  Serial.println("TMP117 test!");
+
+  // Try to initialize!
+  if (!tmp117.begin()) {
+    Serial.println("Failed to find TMP117 chip");
+    while (1) { delay(10); }
+  }
+  Serial.println("TMP117 Found!");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_DIGITAL_SIGNAL, LOW);
-  delay(2);
-  digitalWrite(LED_DIGITAL_SIGNAL, HIGH);
-  delay(2);
+  sensors_event_t temp; // create an empty event to be filled
+  tmp117.getEvent(&temp); //fill the empty event object with the current measurements
+  Serial.print("Temperature  "); Serial.print(temp.temperature);Serial.println(" degrees C");
+  Serial.println("");
+  digitalWriteFast(LED_DIGITAL_SIGNAL, HIGH);
+  delayMicroseconds(2000);
+  digitalWriteFast(LED_DIGITAL_SIGNAL, LOW);
+  delayMicroseconds(20);
+  digitalWriteFast(LED_DIGITAL_SIGNAL, HIGH);
 }
